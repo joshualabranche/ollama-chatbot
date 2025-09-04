06abf7a0f4cd0b25be9c1467c5f02802f7e3f374
joshualabranche <joshua.labranche@sauwce.com>
2025-09-04 16:43:08 -0400

    cleaning up code and adding comments

--- chat-gui.py
+++ chat-gui.py
@@ -5,19 +5,6 @@ Created on Thu Sep  4 13:24:12 2025
 
 @author: jlab
 
-This code creates a new Nicegui window with a chat area and a prompt area, 
-and sets up a button to send messages to an Ollama model. 
-When the user clicks the button, the message in the prompt area is sent 
-to the Ollama model using the `ollama_client.prompt()` method, 
-and the response from the model is displayed in the chat area.
-
-You will need to replace the `ollama_client` variable with your own 
-instance of an Ollama client object, which you can create by calling 
-the `OllamaClient()` constructor and passing in your API key and 
-secret as arguments. You will also need to modify the `on_send_message()` 
-function to handle the response from the Ollama model in a way that makes sense
-for your application.
-
 
 """
 
@@ -26,60 +13,79 @@ from ollama import AsyncClient
 from uuid import uuid4
 from datetime import datetime
 
+# generate a list of message information to keep a chat history with the bot
 message = []
 
 @ui.page('/')
 def main():
     global message
     
+    # generate a bot icon from user ID and human icon from 'human' string
     user_id = str(uuid4())
     robot_avatar = f'https://robohash.org/{user_id}?bgset=bg2'
     user_avatar = 'https://robohash.org/human?bgset=bg2'
     
+    # async function to clear messages such that the chat history is wiped
     async def clear() -> None:
         message.clear()
         ui.notify('History Cleared!')
     
-    message = []
-    async def send() -> None:
-               
+    
+    async def send() -> None:           
+        # text is a ui.input used to get user input
         question = text.value
         text.value = ''
 
         with message_container:
+            # timestamp each message
             stamp = datetime.now().strftime('%X')
+            # create user message input
             ui.chat_message(text=question, stamp=stamp, avatar=user_avatar, name='User', sent=True)
+            # create bot response message
             response_message = ui.chat_message(name='Mr. Robot', stamp=stamp, avatar=robot_avatar, sent=False)
+            # "thinking" dots for output
             spinner = ui.spinner(type='dots')
+            # append chat history to log file
             log.push(question)
 
+        
+        # append current prompt to chat history
         message.append({'role': 'user', 'content': question})
+        # retrieve the response async as a streamed output
         response = ''
         async for part in await AsyncClient().chat(model='codellama:latest', messages=message, stream=True):
             response += part['message']['content']
             response_message.clear()
             with response_message:
+                # response message is output in pure html
                 ui.html(response)
+            # js function to auto scroll as chat develops
             ui.run_javascript('window.scrollTo(0, document.body.scrollHeight)')
+        # once responded no more "thinking" dots
         message_container.remove(spinner)
+        # save bot response to chat history and log tab
         message.append({'role': 'assistant', 'content' : response})
         log.push(response)
         
+    # CSS style options
     ui.add_css(r'a:link, a:visited {color: inherit !important; text-decoration: none; font-weight: 500}')
 
     # the queries below are used to expand the contend down to the footer (content can then use flex-grow to expand)
     ui.query('.q-page').classes('flex')
     ui.query('.nicegui-content').classes('w-full')
 
+    # create two tabs in the window
     with ui.tabs().classes('w-full') as tabs:
         chat_tab = ui.tab('Chat')
         logs_tab = ui.tab('Logs')
     
+    # create message container for the chat tab and log for the log tab
     with ui.tab_panels(tabs, value=chat_tab).classes('w-full max-w-2xl mx-auto flex-grow items-stretch'):
         message_container = ui.tab_panel(chat_tab).classes('items-stretch')
         with ui.tab_panel(logs_tab):
             log = ui.log().classes('w-full h-full')
 
+    # this is where the user input box and clear history button are constructed
     with ui.footer().classes('bg-white'), ui.column().classes('w-full max-w-3xl mx-auto my-6'):
         with ui.row().classes('w-full no-wrap items-center'):
             placeholder = 'message'
@@ -87,4 +93,5 @@ def main():
                 .classes('w-full self-center').on('keydown.enter', send)
             ui.button('Reset History', on_click=clear) #lambda message: (message := [], ui.notify('Chat History Cleared!')))
 
+# lastly we run the GUI
 ui.run(title='LLM Chat', favicon='😊')
